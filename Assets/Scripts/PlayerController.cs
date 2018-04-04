f353cdb6a859b0052de624751da843d122a68fe8
Sanio Vincent <svinc26@hotmail.com>
2018-04-04 21:35:00 +1000

    Introduction
    -Implemented starting stage

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public Rigidbody rB;
+
+	// Use this for initialization
+	void Start () {
+        rB = GetComponent<Rigidbody>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
