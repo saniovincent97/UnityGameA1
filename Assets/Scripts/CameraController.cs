945fe96e05c5028f1478b3b5bf1bf8eeaa8c18be
Sanio Vincent <svinc26@hotmail.com>
2018-04-05 11:48:06 +1000

    Implement camera rotation lock
    Prevents camera from clipping into the ground

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -12,6 +12,13 @@ public class CameraController : MonoBehaviour {
 
     public bool useOffsetValues;
 
+    public Transform pivot;
+
+    public float maxViewAngle;
+    public float minViewAngle;
+
+    public bool invertY;
+
 	// Use this for initialization
 	void Start () {
         if (!useOffsetValues)
@@ -19,19 +26,50 @@ public class CameraController : MonoBehaviour {
             offset = target.position - transform.position;
         }
 
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = target.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
+
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void LateUpdate () {
         float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
         target.Rotate(0, horizontal, 0);
 
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        //pivot.Rotate(vertical, 0, 0);
+        if (invertY)
+        {
+            pivot.Rotate(vertical, 0, 0);
+        } else
+        {
+            pivot.Rotate(-vertical, 0, 0);
+        }
+
+        if(pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180f)
+        {
+            pivot.rotation = Quaternion.Euler(maxViewAngle, 0, 0);
+        }
+
+        if (pivot.rotation.eulerAngles.x > 180 && pivot.rotation.eulerAngles.x < 360f + minViewAngle)
+        {
+            pivot.rotation = Quaternion.Euler(360f + minViewAngle, 0, 0);
+        }
+
+        float desiredXAngle = pivot.eulerAngles.x;
         float desiredYAngle = target.eulerAngles.y;
-        Quaternion rotation = Quaternion.Euler(0, desiredYAngle, 0);
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
         transform.position = target.position - (rotation * offset);
 
         //transform.position = target.position - offset;
+
+        if(transform.position.y < target.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y - .5f, transform.position.z);
+        }
 
         transform.LookAt(target);
 		
